5adc28219580b73c4a2930a174a6d38fa54a5c20
5songyon5@naver.com <5songyon5@naver.com>
2022-11-13 17:21:10 +0900

    first commit

---
+++ src/main/java/hello/hellospring/controller/aop/TimeTraceAop.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller.aop;
+
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Aspect;
+@Component
+@Aspect
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString()+ " " + timeMs +
+                    "ms");
+        }
+    }
+}
